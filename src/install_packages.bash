e4d08666ebf32c8d6bb00d37b2e6774c3a115c76
Pierre Van Hauwaert <pierre@rtech-engineering.nl>
2019-02-24 21:38:44 +0100

    update

---
+++ src/install_packages.bash
@@ -0,0 +1,16 @@
+#!/bin/bash 
+
+# mint 19.1 
+sudo aptitude -y install vim ncftp chromium-browser ssh-agent-filter nfs-common nfs-kernel-server vlc ssh-agent-filter   ssh ssh-agent-filter   htop   libopenmpi-dev openmpi-bin   gir1.2-gtop-2.0   scons   g++   metis   libmetis5   libmetis-dev   cmake   gmusicbrowser   smartmontools   xul-ext-calendar-timezones xul-ext-lightning   libnet-dbus-perl  libnet-dbus-perl      sensors-applet  sensors-applet  python-pip  git  python3-setuptools  python-setuptools python3-tk tcsh  r-base-dev  libcurl4-openssl-dev  ant  python3.7  ristretto  tumbler  evince  colordiff pinta  npm maven python-pytest  tox  python3-pip freeglut3-dev cmake-curses-gui  libtbb-dev  libglfw3-dev
+
+
+
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt update
+sudo apt -y  install oracle-java8-installer
+
+pip3 install wheel --user 
+pip3 install matplotlib  vtk  seaborn scikit-learn xlrd  openpyxl plotly xlwt  pandas  xlrd  xlsxwriter  xlutils  google-api-python-client oauth2client pyqt5 dask  meshio grip --user  
+
+
+ 
